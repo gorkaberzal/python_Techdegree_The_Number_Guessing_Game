675bdb4f91b502ce46c2d3b9789c8a2825c16844
Gorka Berzal <gorka.berzal@gmail.com>
2025-02-16 10:30:56 +0100

    final_changes_ready_to_submit

--- guessing_game.py
+++ guessing_game.py
@@ -14,39 +14,34 @@ def start_game():
 #   When the program starts, we want to:
 #   ------------------------------------
 #   1. Display an intro/welcome message to the player.
-    print("Welcome to our Guessing game")
+    print("------------ Welcome to our Guessing game ------------")
     
 #   2. Store a random number as the answer/solution.
-    random_number = random.randrange(10)
+    random_number = random.randrange(11)
+#    print("The random number is  {}:  ".format(random_number))
     counter = 0
-    guessed_number = 0
 #   3. Continuously prompt the player for a guess.
     while True:
         counter += 1
-
-
-
-        print("The random number is  {}:  ".format(random_number))
 #     a. If the guess is greater than the solution, display to the player "It's lower".
         try:
-            guessed_number = int(input("Please enter a number   "))
-            
-#             if (guessed_number > 10 ):
-#               print("Please enter a positive number between 1 and 10, Thank you   ")    
-        except UnboundLocalError:
-            print(UnboundLocalError)
+            guessed_number = int(input("Please enter a number between 0 and 10 "))
+            if (guessed_number > 10 ):
+                continue
+            elif (guessed_number < 0):
+                continue
         except ValueError:
-            print(ValueError)
+#            print(ValueError)
+                continue
         else: 
             if (guessed_number > random_number):
                 print("It is lower.   ")
     #     b. If the guess is less than the solution, display to the player "It's higher".
             elif (guessed_number < random_number):
                 print("It is higher.   ")
-
     #   4. Once the guess is correct, stop looping, inform the user they "Got it"
             elif (guessed_number == random_number):
-                print("Got It   ")
+                print("\n--------------- Got It ----------------  ")
     #      and show how many attempts it took them to get the correct number.
                 print("It took you {}, attempts  ".format(counter))
                 print ("The guessed number was  {},   ".format(guessed_number))
